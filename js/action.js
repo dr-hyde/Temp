2f54dd2245b15dd2c404a5b63f06b5812d7dd18b
dr-hyde <31983975+dr-hyde@users.noreply.github.com>
2017-10-31 17:33:15 +0530

    JQ for pre-final page

--- js/action.js
+++ js/action.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
 	$(".sign-form").slideUp(0);
+	$(".b-log a").css("opacity", 1);
 	$(".b-sign").click(function(){
-		$(".make-list").slideUp(3000);
-		$(".log-form").slideUp(3000, function(){
+		$(".b-sign a").css("opacity", 1);
+		$(".b-log a").css("opacity", .5);
+		$(".log-form").slideUp(1000);
+		$(".make-list").slideUp(1000, function(){
 			$(".left-side").removeClass("col-sm-7").addClass("col-sm-5");
 			$(".right-side").removeClass("col-sm-5").addClass("col-sm-7");
-			$(".sign-form").slideDown(3000);
+			$(".sign-form").slideDown(1000);
 		});
 	});
 	$(".b-log").click(function(){
-		$(".sign-form").slideUp(3000, function(){
+		$(".b-log a").css("opacity", 1);
+		$(".b-sign a").css("opacity", .5);
+		$(".sign-form").slideUp(1000, function(){
 			$(".right-side").removeClass("col-sm-7").addClass("col-sm-5");
 			$(".left-side").removeClass("col-sm-5").addClass("col-sm-7");
-			$(".log-form").slideDown(3000);
-			$(".make-list").slideDown(3000);
+			$(".log-form").slideDown(1000);
+			$(".make-list").slideDown(1000);
 		});
 	});
 });
